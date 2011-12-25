ea5b7422e4ccf706c265a095f59d4204bac2b9c7
ryunosinfx <ryunosinfx@livedoor.com>
2011-12-25 16:02:10 +0900

    first commit

---
+++ CV/comikeViewerConstants.js
@@ -0,0 +1,64 @@
+const MDATA="MDATA";
+const CDATA="CDATA";
+const CUT_HD="CUTH.CCZ_FILES";
+const CUT_LD="CUTL.CCZ_FILES";
+const CUT_EXTENTION=".PNG";
+const CUT_WIDTH_HD=210;
+const CUT_HEIGHT_HD=300;
+const CUT_WIDTH_LD=105;
+const CUT_HEIGHT_LD=150;
+const INDEX_MAP_X=3;
+const INDEX_MAP_Y=4;
+const VIEW_OFFSET_TOP=4;
+const VIEW_OFFSET_LEFT=2;
+const MAP_PREFIX_HD="HMP";
+const MAP_WIDTH_HD_E="2540";
+const MAP_HEIGHT_HD_E="1080";
+const MAP_WIDTH_HD_W="2000";
+const MAP_HEIGHT_HD_W="1600";
+const MAP_PREFIX_LD="MAP";
+const MAP_WIDTH_LD_E="1270";
+const MAP_HEIGHT_LD_E="540";
+const MAP_WIDTH_LD_W="1000";
+const MAP_HEIGHT_LD_W="800";
+const MAP_WIDTH_SD_E="635";
+const MAP_HEIGHT_SD_E="270";
+const MAP_WIDTH_SD_W="500";
+const MAP_HEIGHT_SD_W="400";
+const MAP_PREFIX_GENRE_HD="HGR";
+const MAP_PREFIX_GENRE_LD="GNR";
+const MAP_RACIO_LD_X=2;//1.27;
+const MAP_RACIO_LD_Y=1;//0.675;
+const MAP_VIEW_ADJUST_Y_PX=2;
+//------------------------------------------------
+const TYPE_COLOR_1="";
+const TYPE_COLOR_2="";
+const TYPE_COLOR_3="";
+const TYPE_COLOR_4="";
+//------------------------------------------------
+const MAX_VIEW_DETAIL_HISTRY_COUNT=1000;
+//------------------------------------------------
+const VIEW_DETAIL_HISTORY = "[TIME] SPACE CNAME CTOP";
+const VIEW_DETAIL_TIME = "MM/DD hh:mm:ss";
+const TITLE_BASE = " YYYYMMDD X日目 SPACE";
+const MSG_FIND_NOT_FOUND = "見つかりませんでした";
+//------------------------------------------------
+const IS_VISIBLE_BACKGROUND_IMAGE=true;
+//------------------------------------------------
+const MAP_WIDTH_HD="MAP_WIDTH_HD";
+const MAP_HEIGHT_HD="MAP_HEIGHT_HD";
+const MAP_WIDTH_LD="MAP_WIDTH_LD";
+const MAP_HEIGHT_LD="MAP_HEIGHT_LD";
+const MAP_WIDTH_SD="MAP_WIDTH_SD";
+const MAP_HEIGHT_SD="MAP_HEIGHT_SD";
+//------------------------------------------------
+const INDEX="INDEX";
+const MAP="MAP";
+const DAY="DAY";
+const BLOCK="BLOCK";
+const GENRE="GENRE";
+//------------------------------------------------
+const nameFormatMain = "C[0-9]{2}ROM.TXT";//本体
+const nameFormatMap = "C[0-9]{2}MAP.TXT";//マップ描画情報
+const nameFormatDef = "C[0-9]{2}DEF.TXT";//各種区分値情報
+const CIRCLE_COUNT_MAX =1000000;
